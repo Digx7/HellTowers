286a7a3ebdb98ac26803a8cfb507dd1b8ad90453
Tello- <tellovisionstudio@gmail.com>
2021-09-21 12:45:39 -0700

    Working on developing a view range component

---
+++ Assets/Scripts/ViewRange.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ViewRange : MonoBehaviour
+{
+    [SerializeField] private FloatType range;
+    [SerializeField] private bool drawViewRange;
+
+
+
+    private void Start()
+    {
+        if (drawViewRange == true)
+        {
+           
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
