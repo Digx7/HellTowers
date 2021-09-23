b1e346d4fb7582b8069d4de3c8184ed20e8ee5c6
Tello- <tellovisionstudio@gmail.com>
2021-09-22 23:39:27 -0700

    end of day commit
    Just a bit of refactoring before bed

--- Assets/Scripts/ViewRange.cs
+++ Assets/Scripts/ViewRange.cs
@@ -6,17 +6,24 @@ using System;
 [RequireComponent(typeof(LineRenderer))]
 public class ViewRange : MonoBehaviour
 {
-    [SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private Color materialColor;
-
     [SerializeField] private bool isRendered;
+    
+    [SerializeField] private Color materialColor;
 
     [SerializeField] private FloatType radius;
     private float minRadius = 0;
     private float maxRadius = 500;
 
 
-    [SerializeField] private IntegerType defaultVertCount;
+    [SerializeField] private IntegerType vertCount;
+    private int minVertCount = 0;
+    private int maxVertCount = 100;
+
+
+    [SerializeField] private LineRenderer lineRenderer;
+
+
+    
     
 
     public bool IsRendered { get => isRendered; set => isRendered = value; }
@@ -28,12 +35,13 @@ public class ViewRange : MonoBehaviour
         if (isRendered)
         {            
             RenderCircle();
+
         }
     }
 
     private void OnValidate()
     {
-        radius.Value = Mathf.Clamp(radius.Value, minRadius, maxRadius); // or int.MaxValue, if you need to use an int but can't use uint.
+        ClampProperties();
     }
 
     private void Update()
@@ -53,9 +61,10 @@ public class ViewRange : MonoBehaviour
 
     void RenderCircle()
     {
+        ClampProperties();
         lineRenderer.enabled = true;
         lineRenderer.loop = true;
-        lineRenderer.positionCount = defaultVertCount.Value + 1;
+        lineRenderer.positionCount = vertCount.Value + 1;
         lineRenderer.useWorldSpace = false;
         lineRenderer.material.color = MaterialColor;
         CreatePoints();
@@ -68,14 +77,20 @@ public class ViewRange : MonoBehaviour
 
         float angle = 20f;
 
-        for (int i = 0; i < (this.defaultVertCount.Value + 1); i++)
+        for (int i = 0; i < (this.vertCount.Value + 1); i++)
         {
             x = Mathf.Sin(Mathf.Deg2Rad * angle) * this.Radius.Value;
             z = Mathf.Cos(Mathf.Deg2Rad * angle) * this.Radius.Value;
 
             lineRenderer.SetPosition(i, new Vector3(x, 0, z));
 
-            angle += (360f / this.defaultVertCount.Value);
+            angle += (360f / this.vertCount.Value);
         }
+    }
+
+    void ClampProperties()
+    {
+        radius.Value = Mathf.Clamp(radius.Value, minRadius, maxRadius); // or int.MaxValue, if you need to use an int but can't use uint.
+        vertCount.Value = Mathf.Clamp(vertCount.Value, minVertCount, maxVertCount);
     }
 }
